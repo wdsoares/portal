7166a4dd1c99f9a684b09cfbfd6cd0afe3e51930
Willian Denner Soares <wsoares@inf.ufsm.br>
2020-12-05 18:55:13 -0300

    UPDATES: é isso aí

--- DatabaseAdonis.cs
+++ DatabaseAdonis.cs
@@ -57,23 +57,83 @@ namespace portal
         }
         public void update(string epc)
         {
-            string sql = "UPDATE products p JOIN barcodes b " +  
-                         "ON p.id = b.product_id JOIN tags t " + 
-                         "ON b.id = t.barcode_id SET p.purchases = p.purchases + 1 " +
-                         "WHERE t.epc =  \""+ epc +"\"";
-            MySqlCommand cmd = new MySqlCommand(sql, _connection);
-            try
+            int barcode_id = this.getBarcodeID(epc);
+            int product_id = this.getProductID(epc);
+
+            string sql = "INSERT INTO saidas(id_produto, epc, barcode_id, created_at, updated_at) VALUES (" + product_id + ", \"" + epc + "\", " + barcode_id + ", now(), now())";
+            if(barcode_id != -1 && product_id != -1)
             {
-                this.openConnection();
-                cmd.ExecuteNonQueryAsync();
-                this.closeConnection();
-            }
-            catch(MySqlException e)
-            {
-                Console.WriteLine(e.Message);
+                MySqlCommand cmd = new MySqlCommand(sql, this._connection);
+                try
+                {
+                    this.openConnection();
+                    cmd.ExecuteNonQueryAsync();
+                }
+                catch(MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    this.closeConnection();
+                }
             }
         }
 
-    
+        public int getBarcodeID(string epc)
+        {
+            string sql = "SELECT barcode_id FROM tags WHERE epc = \"" + epc + "\"";
+            int barcode_id = -1;
+            MySqlDataReader rdr = null;
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, this._connection);
+                this.openConnection();
+                rdr = cmd.ExecuteReader();
+                
+                while(rdr.Read())
+                {
+                    barcode_id = rdr.GetInt32(0);
+                }
+            }
+            catch(MySqlException e){
+                Console.WriteLine(e.Message);
+            }
+            finally{
+                this.closeConnection();
+                rdr.Close();
+            }
+
+            return barcode_id;
+        }
+
+        public int getProductID(string epc)
+        {
+            string sql = "SELECT b.product_id FROM barcodes b JOIN tags t ON b.id = t.barcode_id WHERE t.epc = \"" + epc + "\"";
+            int product_id = -1;
+            MySqlDataReader rdr = null;
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, this._connection);
+                this.openConnection();
+                rdr = cmd.ExecuteReader();
+                
+                while(rdr.Read())
+                {
+                    product_id = rdr.GetInt32(0);
+                }
+            }
+            catch(MySqlException e){
+                Console.WriteLine(e.Message);
+            }
+            finally{
+                this.closeConnection();
+                rdr.Close();
+            }
+
+            return product_id;
+        } 
     }
 }
